876e46bf5d2c88341cf26a4853964e987dc93f7e
Alexis Chavez <alexis.chavez17@outlook.com>
2023-04-16 17:25:19 -0700

    Complete JS Events Event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//function addingEventListener() {
+//}
+
 function addingEventListener() {
-}
+    const button = document.getElementById('button'); // Get the element with ID "button"
+    button.addEventListener('click', function(event) { // Bind event listener to "click" event
+      console.log('Button clicked:', event.target); // Add your event handling logic here
+    });
+  }
+  
